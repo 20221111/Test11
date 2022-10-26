b66570083f34fc644fe37ce26df3885165df9578
gyeonghyang <95530660+gyeonghyang@users.noreply.github.com>
2022-10-27 00:53:22 +0900

    tab

--- app/src/main/java/com/example/test11/CLD.java
+++ app/src/main/java/com/example/test11/CLD.java
@@ -10,26 +10,75 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 
 public class CLD extends AppCompatActivity {
     private static final String TAG = "CLD";
 
+    //달력
     private SimpleDateFormat dateFormatForDisplaying = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREA);
     private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("yyyy년 MM월", Locale.KOREA);
     private SimpleDateFormat dateFormatForMonth2 = new SimpleDateFormat("yyyy-MM", Locale.KOREA);
+    //탭바
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private fragmentAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout=findViewById(R.id.tabs);
+        viewPager=findViewById(R.id.view_pager);
+        adapter=new fragmentAdapter(getSupportFragmentManager(),1);
+
+        //FragmentAdapter에 컬렉션 담기
+        adapter.addFragment(new home());
+        adapter.addFragment(new menu());
+        adapter.addFragment(new search());
+
+        //ViewPager Fragment 연결
+        viewPager.setAdapter(adapter);
+
+        //ViewPager과 TabLayout 연결
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setText("홈");
+        tabLayout.getTabAt(1).setText("검색");
+        tabLayout.getTabAt(2).setText("마이페이지");}
+
+}
+
+/*
+
+
 
 
     @Override
@@ -37,8 +86,22 @@ public class CLD extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        ViewPager2 viewPager2 = findViewById(R.id.view_pager);
 
-        final CompactCalendarView compactCalendarView = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+        //탭바
+        fragmentAdapter adapter = new fragmentAdapter(this);
+        viewPager2.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        tab.setText("Tab " + (position + 1));
+                    }
+                }).attach();
+
+        /*final CompactCalendarView compactCalendarView = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
 
         TextView textView_month = (TextView) findViewById(R.id.textView_month);
         TextView textView_result = (TextView) findViewById(R.id.textView_result);
@@ -182,10 +245,11 @@ public class CLD extends AppCompatActivity {
                 Log.d(TAG, "Month was scrolled to: " + firstDayOfNewMonth);
 
             }
-        });
+        });*/
 
-    }
-}
+    //}
+//}
+
 
 /*public class MainActivity extends AppCompatActivity {
     final String TAG = "calendar test";
