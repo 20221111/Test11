0f37ced6a82e857c83cc804abc3b0fc718e27803
gyeonghyang <95530660+gyeonghyang@users.noreply.github.com>
2022-10-27 23:02:13 +0900

    tab

--- app/src/main/java/com/example/test11/CLD.java
+++ app/src/main/java/com/example/test11/CLD.java
@@ -1,41 +1,14 @@
 package com.example.test11;
 
-import android.annotation.SuppressLint;
-import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.widget.ViewPager2;
 
-import com.github.sundeepk.compactcalendarview.CompactCalendarView;
-import com.github.sundeepk.compactcalendarview.domain.Event;
 import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 import java.util.Locale;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
-import android.os.Bundle;
-
-import com.google.android.material.tabs.TabItem;
-import com.google.android.material.tabs.TabLayout;
 
 
 public class CLD extends AppCompatActivity {
@@ -70,17 +43,18 @@ public class CLD extends AppCompatActivity {
         //ViewPager과 TabLayout 연결
         tabLayout.setupWithViewPager(viewPager);
 
-        tabLayout.getTabAt(0).setText("홈");
+        /*tabLayout.getTabAt(0).setText("홈");
         tabLayout.getTabAt(1).setText("검색");
-        tabLayout.getTabAt(2).setText("마이페이지");}
+        tabLayout.getTabAt(2).setText("마이페이지");*/
 
+        tabLayout.getTabAt(0).setIcon(R.drawable.home);
+        tabLayout.getTabAt(1).setIcon(R.drawable.search);
+        tabLayout.getTabAt(2).setIcon(R.drawable.user);
+
+    }
 }
 
 /*
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
