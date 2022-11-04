50608f46f3707bffd4d0de435508b7cc7eee114e
WooyoungKwon <rnjsdndud00@inha.edu>
2022-11-04 23:14:16 +0900

    FEAT : 로딩화면 생성

---
+++ app/src/main/java/com/example/test11/LoadingActivity.java
@@ -0,0 +1,26 @@
+package com.example.test11;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loding);
+        startLoading();
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
--- app/src/main/java/com/example/test11/MainActivity.java
+++ app/src/main/java/com/example/test11/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+
         //insertData insert = new insertData(); 회원가입 테스트용이었음.
         //insert.execute("http://ec2-13-231-175-154.ap-northeast-1.compute.amazonaws.com:8080/SignUp/"+jm.getId()+"?", "0");
         //commMain cm =new commMain();
