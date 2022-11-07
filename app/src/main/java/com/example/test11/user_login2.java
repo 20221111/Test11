22b8aefaa1f26294aa1bd743bc35232530e3e812
gyeonghyang <95530660+gyeonghyang@users.noreply.github.com>
2022-11-08 01:57:44 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/src/main/res/layout/fragment_home.xml

---
+++ app/src/main/java/com/example/test11/user_login2.java
@@ -0,0 +1,36 @@
+package com.example.test11;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import androidx.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class user_login2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_login2);
+
+        ImageButton pw_change = findViewById(R.id.pw_change);
+        pw_change.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), user_login_pw.class);
+                startActivity(intent);
+            }
+        });
+        ImageButton info_change = findViewById(R.id.info_change);
+        info_change.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), user_login_info.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
